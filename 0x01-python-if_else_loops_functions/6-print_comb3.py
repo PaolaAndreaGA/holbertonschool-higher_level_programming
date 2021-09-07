a6f0c614bcdc4f89a6206c2607b33868441b80cb
PaolaAndreaGA <paga0209@gmail.com>
2021-09-07 21:30:00 +0000

    prints all possible different combinations of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for nl in range(0, 10):
+    for nr in range(nl + 1, 10):
+        if nl == 8 and nr == 9:
+            print("{:d}{:d}, ".format(nl, nr))
+        else:
+            print("{:d}{:d}, ".format(nl, nr), end='')
