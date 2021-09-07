6a250616774c7ec21b92cbcd3b2e7d5339e79196
PaolaAndreaGA <paga0209@gmail.com>
2021-09-07 23:39:26 +0000

    uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     for letter in str:
         letter = ord(str)
         if letter >= 97 and letter <= 122:
-            letter = -32
+            letter -= 32
             letter = chr(letter)
             print("{:c}".format(letter), end='')
             print(" ")
