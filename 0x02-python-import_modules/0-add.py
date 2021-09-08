1088eaf52af1974196f4135abea889b7b91d12de
PaolaAndreaGA <paga0209@gmail.com>
2021-09-08 17:50:00 +0000

    add function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
+from add_0 import add
 
-from add_0  import add
-# from filename whitout.py import function
+
 def main():
-    
     a = 1
     b = 2
     c = add(a, b)
-    print("{} + {} = {}".format(a, b, c)
+    print("{} + {} = {}".format(a, b, c))
 
 if_name_ == "_main_":
-      main()
+    main()
