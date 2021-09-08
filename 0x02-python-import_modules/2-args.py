555c544aaf92dd487ae73aaac896c261d5f815c9
PaolaAndreaGA <paga0209@gmail.com>
2021-09-08 21:13:11 +0000

    list with arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    argc = len(argv) - 1
+if argc == 0:
+    print("0 arguments. ")
+elif argc == 1:
+    print("1 argument: ")
+    print("1: {}".format(sys.argv[1]))
+else:
+    print("{:d} arguments:".format(argc))
+    for count in range(1, argc + 1)
+    print("{}: {}".format(count, sys.argv[1]))
+
+if __name__ == "__main__":
+    main()
