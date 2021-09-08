7281ecc01336f45693f9fc02c2c117d17db03385
PaolaAndreaGA <paga0209@gmail.com>
2021-09-08 21:36:28 +0000

    list with arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ import sys
 
 
 def main():
-    argc = len(argv) - 1
+    argc = len(sys.argv) - 1
     if argc == 0:
-        print("0 arguments. ")
+        print("0 arguments.")
     elif argc == 1:
-        print("1 argument: ")
+        print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     else:
         print("{:d} arguments:".format(argc))
