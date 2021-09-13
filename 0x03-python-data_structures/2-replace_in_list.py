8572996b15e1d0d221f66df4e91c680c25cafcb7
PaolaAndreaGA <paga0209@gmail.com>
2021-09-13 18:36:07 +0000

    repalce list v1

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,12 @@
 
 
 def replace_in_list(my_list, idx, element):
-    if idx < 0 and idx >= len(my_list):
-        print("{d}".format(my_list))
-    my_list = element
-    return my_list[idx]
+    if idx < 0:
+        return my_list
+
+    if idx >= len(my_list):
+        return my_list
+
+    else:
+        my_list[idx] = element
+        return my_list
