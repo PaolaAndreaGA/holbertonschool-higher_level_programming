27f8af27180c9aec663b9aa6bfb7ce542311edc7
PaolaAndreaGA <paga0209@gmail.com>
2021-09-13 23:01:47 +0000

    matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+
+    for i in range(matrix):
+        for j in range(i):
+            print("{:d}".format(i), end=''):
+            if j != (i[len(i)-1]):
+                print('', end=' ')
+        print('')
+         
