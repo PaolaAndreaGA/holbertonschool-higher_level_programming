1b60d1509402e47ca852d6ebb889e4344dca9f02
PaolaAndreaGA <paga0209@gmail.com>
2021-09-13 22:21:23 +0000

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,7 +4,7 @@
 def no_c(my_string):
     Nstr = ""
     size = len(my_string)
-    for i in range(0, size):
+    for i in range(size):
         if my_string[i] != "c" and my_string[i] != "C":
                 Nstr += my_string[i]
     return Nstr
