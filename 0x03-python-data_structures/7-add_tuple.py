3b3c557062a126cb73fb2ff6e613e31222d01f6e
PaolaAndreaGA <paga0209@gmail.com>
2021-09-14 00:52:51 +0000

    add tuple

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    size_a = len(tuple_a)
+    size_b = len(tuple_b)
+    if size_a < 2:
+        if size_a == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = (tuple_a[0], 0)
+    if size_b < 2:
+        if size_b == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = (tuple_b[0], 0)
+    tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return tuple_c
