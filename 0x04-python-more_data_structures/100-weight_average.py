f42a5b0c3cfd62f2f2af1f11cae544cacfe16bb1
PaolaAndreaGA <paga0209@gmail.com>
2021-09-15 03:59:35 +0000

    weight_average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    return(list(map(lambda x: list(map(lambda y: y * y, x)), matrix)))
+def weight_average(my_list=[]):
+    if my_list:
+        weight = 0
+        denom = 0
+        avg = 0
+        for i in range(len(my_list)):
+            for j in range(len(my_list[i])):
+                weight += my_list[i][0] * my_list[i][1]
+                denom += my_list[i][1]
+        avg = weight / denom
+        return avg
+    return 0
