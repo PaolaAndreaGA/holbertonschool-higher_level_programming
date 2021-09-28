d3f278b9c7e3ff4dc621cd5a15c5fa41c49cffe6
PaolaAndreaGA <paga0209@gmail.com>
2021-09-28 22:03:36 +0000

    1-square.

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Square class based 0-square"""
 
+
 class Square:
-"""define Square class"""
-    def __init__(self,size):
-    """initilization Square size"""
+    """define Square class"""
+    def __init__(self, size):
+        """initilization Square size"""
         self.__size = size
-  
