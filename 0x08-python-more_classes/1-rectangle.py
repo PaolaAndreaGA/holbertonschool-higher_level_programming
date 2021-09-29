c36738268d4ab099861388a921377e86241ffcc6
PaolaAndreaGA <paga0209@gmial.com>
2021-09-29 15:30:34 -0500

    1-rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Write a class Rectangle that defines a rectangle by: (based on 0-rectangle)"""
+
+
+class Rectangle:
+    """class rectangule 0-rectangle"""
+    def __init__(self, width=0, height=0):
+        """inicialize method"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def width(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise TypeError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def heigth(self, value):
+        if type(value) is not int:
+            raise TypeError("heigth must be an integer")
+        if value < 0:
+            raise TypeError("heigth must be >= 0")
+        self.__height = value
