c2e2bfc4fc2c020f0759bca964183a3e9e1ba5c1
PaolaAndreaGA <paga0209@gmail.com>
2021-09-29 03:26:14 +0000

    print square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Square class based 3-square"""
+
+
+class Square:
+    """define Square class"""
+
+    def __init__(self, size=0):
+        """initilization Square size"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """to retrieve it"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """to set it: size must be int, if size is less than 0"""
+
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+                raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+            """calculate the area of a square"""
+
+            return (self.__size * self.__size)
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+        if self.__size > 0:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#")
+                print("")
+        if self.__size == 0:
+            print("")
