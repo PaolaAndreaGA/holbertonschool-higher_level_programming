b4b99faa13b7db5fbbc30f97273f3e5694350c50
PaolaAndreaGA <paga0209@gmial.com>
2021-10-07 13:25:22 -0500

    fixed

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-
-"""Unittest for max_integer(list=[..])
+"""Unittest for max_integer([..])
 """
 import unittest
-
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """Test 6-max_integer
+    """Runs tests for 6-max_integer
     """
+
     def test_if_empty(self):
         """Checks to make sure a list is empty"""
         empty = []
@@ -17,7 +16,7 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_unordered(self):
         """Checks an unordered list"""
-        un_list = [8, 6, 2, 7, 3]
+        un_list = [1, 5, 2, 8, 3]
         self.assertEqual(max_integer(un_list), 8)
 
     def test_ordered(self):
@@ -27,47 +26,47 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_neg_unordered(self):
         """Checks an unordered list of negatives"""
-        nun_list = [-9, -6, -4, -8, -3]
-        self.assertEqual(max_integer(nun_list), -9)
+        nun_list = [-1, -5, -2, -8, -3]
+        self.assertEqual(max_integer(nun_list), -1)
 
     def test_neg_ordered(self):
         """Checks an ordered list of negatives"""
-        no_list = [-7, -6, -5, -4, -2]
-        self.assertEqual(max_integer(no_list), -2)
+        no_list = [-1, -2, -3, -4, -5]
+        self.assertEqual(max_integer(no_list), -1)
 
     def test_single(self):
         """Checks a list with a single element"""
-        single = [6]
-        self.assertEqual(max_integer(single), 6)
+        single = [5]
+        self.assertEqual(max_integer(single), 5)
 
     def test_float_list(self):
         """Checks a list of floats"""
-        f_list = [2.2, 5.5, 6.3, 8.9, 2.7]
-        self.assertEqual(max_integer(f_list), 2.7)
+        f_list = [3.2, 5.6, 7.3, 9.9, 12.7]
+        self.assertEqual(max_integer(f_list), 12.7)
 
     def test_float_int(self):
         """Checks a list of floats and ints"""
-        f_and_i = [2.2, 1, 7.4, 9, 3.4]
-        self.assertEqual(max_integer(f_and_i), 7)
+        f_and_i = [2.2, 1, 7.4, 8, 3.4]
+        self.assertEqual(max_integer(f_and_i), 8)
 
     def test_string_list(self):
         """Checks a list of strings"""
-        s_list = ["oh", "hello", "beato", "burrito"]
-        self.assertRaises(TypeError, max_integer, "beato", s_list)
+        s_list = ["oh", "hello", "neato", "burrito"]
+        self.assertRaises(TypeError, max_integer, "neato", s_list)
 
     def test_single_string(self):
         """Checks a single string"""
-        single_string = ["house"]
-        self.assertRaises(TypeError, max_integer, "s", single_string)
+        single_string = ["rito"]
+        self.assertRaises(TypeError, max_integer, "r", single_string)
 
     def test_same(self):
         """Checks through a list of the same ints"""
-        same_int = [30, 30, 30, 30]
-        self.assertEqual(max_integer(same_int), 30)
+        same_int = [22, 22, 22, 22]
+        self.assertEqual(max_integer(same_int), 22)
 
     def test_mixed_types(self):
         """Checks to see if numbers are mixed with strings"""
-        mixed = [1, 2, 3, "beat", 5]
+        mixed = [1, 2, 3, "neat", 5]
         self.assertRaises(TypeError, max_integer, mixed)
 
 if __name__ == '__main__':
