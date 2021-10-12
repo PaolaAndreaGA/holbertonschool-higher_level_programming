accf9f294966a233804ae7559720d0e6099a9771
PaolaAndreaGA <paga0209@gmial.com>
2021-10-11 21:11:54 -0500

    8-rectangle.py

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Write an class
+"""
+
+
+class BaseGeometry:
+    """Class that defines a shape
+    """
+
+    pass
+
+    def area(self):
+        """Calculates Area
+        """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """function valide type object
+        """
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """class Rectangle
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width",width)
+        self.__width = width
+
+        self.integer_validator("heigth",height)
+        self.__heigth = height
