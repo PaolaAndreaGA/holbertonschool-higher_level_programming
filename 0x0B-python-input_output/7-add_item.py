426727b08a79a8120ac486daa1d0ed53ff77cdce
PaolaAndreaGA <paga0209@gmial.com>
2021-10-12 17:43:37 -0500

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,9 +9,6 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-present = os.path.isfile(filename)
-
-filename = "add_item.json"
 try:
     new = load_from_json_file(filename)
 except (ValueError, FileNotFoundError):
