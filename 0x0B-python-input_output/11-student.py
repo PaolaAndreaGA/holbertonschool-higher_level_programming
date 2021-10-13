e87399510613b944d16c3b74d1dfdfdbeafdc9f6
PaolaAndreaGA <paga0209@gmial.com>
2021-10-13 16:32:59 -0500

    11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -11,22 +11,22 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """Public method Retrieves a dictionary
-        representation of a Student instance"""
-        my_dict = {}
-        if attrs is None:
+        """Public method to retrieve a dictionary representation
+            of a student instance
+        """
+        if attrs is not None:
+            dict_student = {}
+            for k in self.__dict__:
+                if k in attrs:
+                    dict_student[k] = self.__dict__[k]
+            return dict_student
+        else:
             return self.__dict__
-        for attr in attrs:
-            if attr in self.__dict__.keys():
-                my_dict[attr] = self.__dict__[attr]
-        return my_dict
 
     def reload_from_json(self, json):
-        """Public method that replaces all
-        attributes of the Student instance"""
-        my_old_dict = self.__dict__
-        for attr in json.keys():
-            for my_old_attr in my_old_dict.keys():
-                if attr == my_old_attr:
-                    my_old_dict[my_old_attr] = json[attr]
-            return my_old_dict
+        """Replaces all attributes of the Student instance
+        Args:
+            json (dict): instance dictionary
+        """
+        if json:
+            self.__dict__ = json
