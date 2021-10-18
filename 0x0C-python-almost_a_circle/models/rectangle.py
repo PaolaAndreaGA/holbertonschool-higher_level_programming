94cbcf41f426cd61d553905dca757f2bf36a309b
PaolaAndreaGA <paga0209@gmial.com>
2021-10-17 22:35:30 -0500

    fixes pep8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,79 +1,104 @@
 #!/usr/bin/python3
-"""create a rectangle"""
-
-
+"""
+Class Module
+"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Create Rectangle class"""
+    """Rectangle subclass"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """init method"""
+        """instance initialization method
+
+        args:
+            width: width of rectangle
+            height: height of rectangle
+            x: init variable
+            y: init variable
+        """
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
+
+    def area(self):
+        """return area of width & height"""
+        return self.width * self.height
+
+    def update(self, *args, **kwargs):
+        """update attributes"""
+        if args:
+            listme = ['id', 'width', 'height', 'x', 'y']
+            i = 0
+            for arg in args:
+                setattr(self, listme[i], arg)
+                i += 1
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def display(self):
+        """print into stdout
+        return: na
+        """
+        for row in range(self.y):
+            print()
+        for row in range(self.height):
+            print("{}{}".format(" " * self.x, "#" * self.width))
+
+    def __str__(self):
+        """print method"""
+        return "[{}] ({}) {}/{} - {}/{}".format(self.__class__.__name__,
+                                                self.id, self.__x, self.__y,
+                                                self.__width, self.__height)
+
+    def to_dictionary(self):
+        """return dict representation of Rectangle"""
+        return {'x': self.x, 'y': self.y, 'id': self.id,
+                'height': self.height, 'width': self.width}
 
     @property
     def width(self):
-        """return width as private attribute"""
+        """width getter method"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """adding validation of all setter
-     methods and instantiation"""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
+        """width setter method"""
+        self.integer_validator('width', value)
         self.__width = value
 
     @property
     def height(self):
-        """return height as private attribute"""
+        """height getter method"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """adding validation of all setter
-     methods and instantiation"""
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
+        """height setter method"""
+        self.integer_validator('height', value)
         self.__height = value
 
     @property
     def x(self):
-        """return x as private attribute"""
+        """x getter method"""
         return self.__x
 
+    @x.setter
     def x(self, value):
-        """adding validation of all setter
-     methods and instantiation"""
-        if type(value) is not int:
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
+        """x setter method"""
+        self.integer_validator2('x', value)
         self.__x = value
 
     @property
     def y(self):
-        """return y as private attribute"""
+        """y getter method"""
         return self.__y
 
-    def height(self, value):
-        """adding validation of all setter
-     methods and instantiation"""
-        if type(value) is not int:
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
+    @y.setter
+    def y(self, value):
+        """y setter method"""
+        self.integer_validator2('y', value)
         self.__y = value
-
-    def area(self):
-        """calcules rectangule area"""
-        return self.width * self.__height
