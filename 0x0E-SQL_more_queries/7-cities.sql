8a74ae1fd29fce16089429d64a1c88b06a025114
PaolaAndreaGA <paga0209@gmial.com>
2021-11-25 11:13:35 -0500

    7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,8 +3,8 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
-       id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
-       name VARCHAR(256) NOT NULL,
-       state_id INT NOT NULL,
-       FOREIGN KEY (state_id) REFERENCES state(id)
+    id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id),
+    name VARCHAR(256) NOT NULL
 );
