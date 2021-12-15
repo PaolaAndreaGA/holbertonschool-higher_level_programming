bed8ff0e30f1f5400f176ea24720bc2f8b5f12e5
PaolaAndreaGA <paga0209@gmial.com>
2021-12-15 10:07:19 -0500

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,9 +1,15 @@
 #!/usr/bin/node
-const Squaree = require('./5-square');
-class Square extends Squaree {
-  charPrint (letter = 'X') {
-    super.print(letter);
+
+const squareB = require('./5-square');
+class Square extends squareB {
+  charPrint (c) {
+    if (!c) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
   }
 }
-
 module.exports = Square;
