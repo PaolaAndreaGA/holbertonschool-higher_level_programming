0ab201fdb2b2ccbb36c83930b74707025139dc4a
PaolaAndreaGA <paga0209@gmial.com>
2021-12-19 12:07:15 -0500

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
-'''module 0 '''
+""" Simple list of table states """
 
 import sys
 import MySQLdb
 
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
+    connection = MySQLdb.connect(
+        host='localhost',
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8"
-    )
+        db=sys.argv[3]
+        )
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    query_rows = cur.fetchall()
-
-    for row in query_rows:
+    holder = connection.cursor()
+    query = "SELECT * FROM states ORDER BY id ASC"
+    holder.execute(query)
+    rows = holder.fetchall()
+    for row in rows:
         print(row)
-    cur.close()
-    db.close()
+    holder.close()
