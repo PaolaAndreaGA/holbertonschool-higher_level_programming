2d58e6a7f8ed0d62609d5d64b63ea9a711739ef0
PaolaAndreaGA <paga0209@gmial.com>
2021-12-19 13:02:10 -0500

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,27 +5,28 @@
 def getStates(userName, passWord, dbName):
     """lists all states from the database """
 
-        import MySQLdb
-        db = MySQLdb.connect(
-             host="localhost",
-             port=3306,
-             user=username,
-             passwd=password,
-             db=dbName,
-             charset="utf8"
-        )
+    import MySQLdb
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=dbName,
+        charset="utf8"
+    )
 
-        cur = db.cursor()
-        cur.execute("SELECT * FROM states ORDER BY id ASC;")
-        query_rows = cur.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC;")
+    query_rows = cur.fetchall()
 
-        for row in query_rows:
-            print(row)
+    for row in query_rows:
+        print(row)
         cur.close()
         db.close()
 
-    if __name__ == "__main__":
-        """arguments and passes to get states from db"""
-        from sys import argv
 
-        getStates(argv[1], argv[2], argv[3])
+if __name__ == "__main__":
+    """arguments and passes to get states from db"""
+    from sys import argv
+
+    getStates(argv[1], argv[2], argv[3])
