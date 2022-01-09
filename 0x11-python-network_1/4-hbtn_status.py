d5026c0c50287af2ce19dd5a6a8275adfb4ff6c9
PaolaAndreaGA <paga0209@gmial.com>
2022-01-09 12:02:42 -0500

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,8 +3,10 @@
 
 
 if __name__ == "__main__":
+
     import requests
-    req = requests.get(' https://intranet.hbtn.io/status')
+    req = requests.get('https://intranet.hbtn.io/status')
     text = req.text
-    print("Body response:\n\t- type: {}".format(type(text)))
+    print("Body response:")
+    print("\t- type: {}".format(type(text)))
     print("\t- content: {}".format(text))
