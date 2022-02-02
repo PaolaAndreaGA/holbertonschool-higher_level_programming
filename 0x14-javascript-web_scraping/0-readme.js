44aa2f23e05ed03d3dc6f76fd8e29ee451deef22
Paola Andrea García Altamirano <85592476+PaolaAndreaGA@users.noreply.github.com>
2022-02-01 21:05:36 -0500

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const Demo = process.argv[2];
-fs.readFile(Demo, 'utf8', function (err, data) {
+fs.readFile(Demo, 'utf8', function(err, data) {
   if (err) {
     console.log(err);
   } else {
