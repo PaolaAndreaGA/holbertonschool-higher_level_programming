e02c596b44717ae700ab658f701016f5f285067d
PaolaAndreaGA <paga0209@gmial.com>
2022-02-01 21:22:47 -0500

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const demo = process.argv[2];
-fs.readFile(demo, 'utf8', function(err, data) {
+fs.readFile(demo, 'utf8', function (err, data) {
     if (err) {
 	console.log(err);
     } else {
