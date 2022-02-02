801eb434b1f3f06f4dab9f3081d26047a86cdd4f
PaolaAndreaGA <paga0209@gmial.com>
2022-02-01 20:38:44 -0500

    0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const demo = process.argv[2];
+fs.readFile(demo, 'utf8', function(err, data) {
+    if (err) {
+	console.log(err);
+    } else {
+	console.log(data);
+    }
+});
